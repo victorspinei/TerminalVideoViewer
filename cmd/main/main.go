b6e2761316bc651a43ad544e08ec1fc3fd65514a
Victor <93835023+victor247k@users.noreply.github.com>
2024-07-26 01:11:33 +0300

    messing around with images in the terminal

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,9 +1,60 @@
 package main
 
 import (
-	"fmt"
+	//"fmt"
+	//"image/jpeg"
+	//"os"
 )
 
 func main() {
-	fmt.Println("Hello world")
+//	imageFile, err := os.Open("assets/fiona.jpg")
+//	if err != nil {
+//		fmt.Println("couldnt open image")
+//		return
+//	}
+//
+//	defer imageFile.Close()
+//
+//	imageFile.Seek(0, 0)
+//
+//	loadedImage, err := jpeg.Decode(imageFile)
+//	if err != nil {
+//		fmt.Println("couldnt decode image")
+//		return
+//	}
+//	width := loadedImage.Bounds().Dx()
+//	height := loadedImage.Bounds().Dy()
+//	scale := 8
+//	verticalScale := scale * 2
+//
+//	fmt.Println(width / scale, height / verticalScale)
+//
+//	for j := 0; j < height; j += verticalScale { // Iterate through height first
+//		for i := 0; i < width; i += scale { // Iterate through width
+//			avgR := 0
+//			avgG := 0
+//			avgB := 0
+//			count := 0
+//			for r := 0; r < verticalScale; r++ {
+//				for c := 0; c < scale; c++ {
+//					ir := i + c // Corrected iteration for width
+//					jc := j + r // Corrected iteration for height
+//					if ir < width && jc < height {
+//						r, g, b, _ := loadedImage.At(ir, jc).RGBA()
+//						avgR += int(r >> 8) // Scale down from 0-65535 to 0-255
+//						avgG += int(g >> 8)
+//						avgB += int(b >> 8)
+//						count++
+//					}
+//				}
+//			}
+//			avgR /= count
+//			avgG /= count
+//			avgB /= count
+//			fmt.Printf("\033[48;2;%d;%d;%dm ", avgR, avgG, avgB)
+//		}
+//		fmt.Printf("\033[m\n")
+//	}
+
+
 }
