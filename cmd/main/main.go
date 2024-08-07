a35e962b8b2cdb95beabf132224fe1774d326820
Victor <93835023+victor247k@users.noreply.github.com>
2024-08-07 20:42:24 +0300

    initial tui example from bubble tea

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,51 +1,63 @@
 package main
 
 import (
+	//	"fmt"
+	//	"sync"
+	//	"time"
+	//
+	//	"github.com/victor247k/TerminalVideoViewer/internal/audio"
 	"fmt"
-	"sync"
-	"time"
+	"os"
 
-	"github.com/victor247k/TerminalVideoViewer/internal/audio"
 	"github.com/victor247k/TerminalVideoViewer/internal/download"
 	"github.com/victor247k/TerminalVideoViewer/internal/extractvideoframes"
 	"github.com/victor247k/TerminalVideoViewer/internal/render"
+	"github.com/victor247k/TerminalVideoViewer/internal/tui"
+
+	tea "github.com/charmbracelet/bubbletea"
 )
 
-var horizontal_scale int = 4
-const factor float64 = 2.5
-var vertical_scale int = int(float64(horizontal_scale) * factor)
-var numWorkers int = 8
+const (
+	//horizontal_scale int = 4
+	//factor float64 = 2.5
+	//vertical_scale int = int(float64(horizontal_scale) * factor)
+	//numWorkers int = 8
+)
 
 func main() {
-	clean()
-
-	var link string
-
-	fmt.Println("Paste your YouTube link:")
-	fmt.Scan(&link)
-
-	download.DownloadFromYoutubeLink(link)
-
-	extractvideoframes.ExtractVideoFrames()
-
-	var wg sync.WaitGroup
-	wg.Add(2)
-
-	go func() {
-		defer wg.Done()
-		frameCount := extractvideoframes.GetFrameCount()
-		fps := download.GetFps()
-		frameDuration := time.Second / time.Duration(fps)
-
-		render.Render(frameCount, frameDuration, horizontal_scale, vertical_scale, numWorkers, fps)
-	}()
-	go func() {
-		defer wg.Done()
-		audio.PlayAudio("temp/audio.mp3")
-	}()
-
-	wg.Wait()
-
+//  clean()
+	p := tea.NewProgram(tui.InitialModel())	
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Alas, there's been an error: %v", err)
+        os.Exit(1)
+	}
+//	var link string
+//
+//	fmt.Println("Paste your YouTube link:")
+//	fmt.Scan(&link)
+//
+//	download.DownloadFromYoutubeLink(link)
+//
+//	extractvideoframes.ExtractVideoFrames()
+//
+//	var wg sync.WaitGroup
+//	wg.Add(2)
+//
+//	go func() {
+//		defer wg.Done()
+//		frameCount := extractvideoframes.GetFrameCount()
+//		fps := download.GetFps()
+//		frameDuration := time.Second / time.Duration(fps)
+//
+//		render.Render(frameCount, frameDuration, horizontal_scale, vertical_scale, numWorkers, fps)
+//	}()
+//	go func() {
+//		defer wg.Done()
+//		audio.PlayAudio("temp/audio.mp3")
+//	}()
+//
+//	wg.Wait()
+//
 	clean()
 }
 
--- go.mod
+++ go.mod
@@ -10,11 +10,24 @@ require (
 
 require (
 	github.com/bitly/go-simplejson v0.5.1 // indirect
+	github.com/charmbracelet/bubbletea v0.26.6 // indirect
+	github.com/charmbracelet/x/ansi v0.1.2 // indirect
+	github.com/charmbracelet/x/input v0.1.0 // indirect
+	github.com/charmbracelet/x/term v0.1.1 // indirect
+	github.com/charmbracelet/x/windows v0.1.0 // indirect
 	github.com/dlclark/regexp2 v1.11.0 // indirect
 	github.com/dop251/goja v0.0.0-20240220182346-e401ed450204 // indirect
 	github.com/ebitengine/purego v0.7.0 // indirect
+	github.com/erikgeiser/coninput v0.0.0-20211004153227-1c3628e74d0f // indirect
 	github.com/go-sourcemap/sourcemap v2.1.3+incompatible // indirect
 	github.com/google/pprof v0.0.0-20240227163752-401108e1b7e7 // indirect
-	golang.org/x/sys v0.18.0 // indirect
+	github.com/mattn/go-localereader v0.0.1 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/muesli/ansi v0.0.0-20230316100256-276c6243b2f6 // indirect
+	github.com/muesli/cancelreader v0.2.2 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
+	golang.org/x/sync v0.7.0 // indirect
+	golang.org/x/sys v0.21.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 )
